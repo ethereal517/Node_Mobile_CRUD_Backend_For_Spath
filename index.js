68cad8a96674ccf244bf9ffc2629185902d8002c
SmartGuy <adam.lindberg520@gmx.com>
2017-02-15 17:10:46 +0100

    body-parser used for note post

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
-
-var express = require('express');
 var app = express();
 var stormpath = require('express-stormpath');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(stormpath.init(app, {
     expand: {
@@ -15,4 +17,13 @@ app.use(stormpath.init(app, {
 app.get('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
     res.json({notes: req.user.customData.notes || 'This is your notebook'});
 });
+app.post('/notes', stormpath.apiAuthenticationRequired, function(req, res) {
+    if (!req.body.notes || typeof req.body.notes != "string") {
+        res.status(400).send("400 Bad Request");
+    }
+    req.user.customData.notes = req.body.notes;
+    req.user.customData.save();
+    res.status(200).end();
+});
+
 app.listen(3000);
